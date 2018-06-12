daf03ac1e95a53d5db5a354be9602f0447f7af9e
ellis-madagan <40174469+ellis-madagan@users.noreply.github.com>
2018-06-12 16:23:11 -0400

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,3 @@
 Write-Output "Starting build..."
 Write-Output "Starting dotnet CLI tool."
-Set-Location -Path C:\Users\emadagan\Desktop\helloworld
-dotnet.exe build --output /build_output
-Write-Output "Running built program..."
-dotnet.exe /build_output/helloworld.dll
+dotnet.exe build
